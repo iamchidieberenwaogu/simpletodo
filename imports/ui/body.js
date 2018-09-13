cb406d00bdaf6fd777eb15415f1ef30cdcd47c19
iamchidieberenwaogu <42776743+iamchidieberenwaogu@users.noreply.github.com>
2018-09-13 15:32:42 +0000

    last modification

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -18,10 +18,13 @@ Template.body.helpers({
         return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
       }
       // Otherwise, return all of the tasks
-      return Tasks.find({}, { sort: { createdAt: -1 } });
-  },
+    return Tasks.find({}, { sort: { createdAt: -1 } });
+},
+incompleteCount() {
+  return Tasks.find({ checked: { $ne: true } }).count();
+},
 });
- 
+
 Template.body.events({
     'submit .new-task'(event) {
       // Prevent default browser form submit
